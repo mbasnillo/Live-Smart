25307bbafebfe7c32f0bd82d100838f181195e6e
milestrong <milesbasnillo@gmail.com>
2017-03-30 22:58:28 +0800

    added materialize files and sample questions for computations

--- frontend/res/js/computecarbon.js
+++ frontend/res/js/computecarbon.js
@@ -1,6 +1,14 @@
 'use strict';
 
 $(document).ready(function(){
+	$('#btn_compute').click(function(){
+			var test = $('input[name="food1"]:checked').val();
+			console.log(test);
+			var test = $('input[name="food2"]:checked').val();
+			console.log(test);
+
+	});
+
 	$('#btn_back').click(function(){
 		window.location.href = '../portal.html';
 	});
