38eeccfe15a2831c5e144191aed0d9c302ba7fde
milestrong <milesbasnillo@gmail.com>
2017-04-03 01:43:56 +0800

    Added initial questions for survey

--- frontend/res/js/survey.js
+++ frontend/res/js/survey.js
@@ -2,6 +2,19 @@
 
 $(document).ready(function(){
 
+  $('#btn_submit').click(function(){
+    var test = $('#form_age').val();
+    console.log(test);
+    var test = $('input[name="sex"]:checked').val();
+    console.log(test);
+    //var test = $('input[name="civil_status"]:checked').val();
+    //console.log(test);
+    var test = $('input[name="education"]:checked').val();
+    console.log(test);
+    var test = $('input[name="salary"]:checked').val();
+    console.log(test);
+  });
+
   $('#btn_back').click(function(){
 		window.location.href = '../portal.html';
 	});
