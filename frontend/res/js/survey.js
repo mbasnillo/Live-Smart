cfcb8ef6cfc3cf9c1d50d0b47220e4168fe54e34
milestrong <milesbasnillo@gmail.com>
2017-04-04 22:31:28 +0800

    added survey questionnaire

--- frontend/res/js/survey.js
+++ frontend/res/js/survey.js
@@ -3,6 +3,7 @@
 $(document).ready(function(){
 
   $('#btn_submit').click(function(){
+    /*
     var test = $('#form_age').val();
     console.log(test);
     var test = $('input[name="sex"]:checked').val();
@@ -13,6 +14,17 @@ $(document).ready(function(){
     console.log(test);
     var test = $('input[name="salary"]:checked').val();
     console.log(test);
+    */
+
+    var score = 0;
+    for(var i = 1; i<=15; i++){
+      var question = "q" + i;
+      var res = $('input[name='+question+']:checked').val();
+      res = res * 1;
+      score += res;
+    }
+    console.log(score);
+
   });
 
   $('#btn_back').click(function(){
