308d6c9a475edeb9afcdc0bf6c2dd196c8df93f8
milestrong <milesbasnillo@gmail.com>
2017-05-03 23:09:38 +0800

    pushing previous unpushed changes

--- frontend/res/js/result.js
+++ frontend/res/js/result.js
@@ -16,7 +16,42 @@ $(document).ready(function(){
       return console.log(err);
     }
   }).done(function(data){
-    $('#score').html(data[0].carbon_level);
+    var food_score = data[0].food_score;
+    var travel_score = data[0].travel_score;
+    var home_score = data[0].home_score;
+    var carbon_level = data[0].carbon_level;
+    $('#score').html(carbon_level);
+    if(carbon_level > 0 && carbon_level <= 3){
+      $('#evaluation').html("Seems like you're doing a great job at helping the environment. Keep it up!");
+    }else if(carbon_level > 3 && carbon_level <= 5){
+      $('#evaluation').html("Seems like you've been doing a decent job, though you could use some work in some areas.");
+    }else if(carbon_level > 5 && carbon_level <= 8){
+      $('#evaluation').html("Seems like you've got some work to do to lessen your carbon emissions. Why not take a look at our guides?");
+    }else{
+      $('#evaluation').html("Seems like you've got a lot of work to do to contribute to helping the environment. Refer to our guides to learn how to reduce your carbon emissions.");
+    }
+
+    if(food_score == travel_score && food_score == home_score){
+      $('#suggestion').html("Work on travel");
+    }else{
+      if(food_score > travel_score){
+        if(food_score > home_score){
+          //food is highest
+          $('#suggestion').html("Work on food");
+        }else{
+          //home is highest
+          $('#suggestion').html("Work on home");
+        }
+      }else{
+        if(travel_score > home_score){
+          //travel is highest
+          $('#suggestion').html("Work on travel");
+        }else{
+          //home is highest
+          $('#suggestion').html("Work on home");
+        }
+      }
+    }
   });
 
   $('#btn_back').click(function(){
